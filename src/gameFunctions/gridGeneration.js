a87602f6ff29bcbe9603b98b2e6767dadbeae2fc
Troy Budd <troy_budd95@msn.com>
2020-02-14 12:43:27 +0000

    TB - Created increment around bombs function, formated and refactored

--- src/gameFunctions/gridGeneration.js
+++ src/gameFunctions/gridGeneration.js
@@ -34,22 +34,65 @@ function generateBombs(width, height, bombs) {
 
 function generateGrid(width, height, bombs) {
 	let gridArr = create2DArray(height);
-	const bombsCoords = generateBombs(width, height, bombs);
+	const bombsCoordArr = generateBombs(width, height, bombs);
 
-    (function setBasicGrid() {
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                gridArr[y][x] = 0;
-            }
-        }
-    })()
-	
+	(function setBasicGrid() {
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				gridArr[y][x] = 0;
+			}
+		}
+	})();
+
 	(function plotBombs() {
-		bombsCoords.forEach(bombLocation => {
-			gridArr[bombLocation[0]][bombLocation[1]] = true;
+		bombsCoordArr.forEach(locationArr => {
+			gridArr[locationArr[0]][locationArr[1]] = true;
 		});
 	})();
-	return gridArr;
+
+	return incrementAroundBombs(width, height, gridArr);
 }
 
-
+function incrementAroundBombs(width, height, gridArr) {
+	let fullGrid = [...gridArr];
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			// If bomb increment all coords around it
+			if (fullGrid[y][x] === true) {
+				// north
+				if (y + 1 !== height && fullGrid[y + 1][x] !== true) {
+					fullGrid[y + 1][x] += 1;
+				}
+				// north east
+				if (y + 1 !== height && x + 1 !== width && fullGrid[y + 1][x + 1] !== true) {
+					fullGrid[y + 1][x + 1] += 1;
+				}
+				// east
+				if (x + 1 !== width && fullGrid[y][x + 1] !== true) {
+					fullGrid[y][x + 1] += 1;
+				}
+				// south east
+				if (y - 1 !== -1 && x + 1 !== width && fullGrid[y - 1][x + 1] !== true) {
+					fullGrid[y - 1][x + 1] += 1;
+				}
+				// south
+				if (y - 1 !== -1 && fullGrid[y - 1][x] !== true) {
+					fullGrid[y - 1][x] += 1;
+				}
+				// south west
+				if (y - 1 !== -1 && x - 1 !== -1 && fullGrid[y - 1][x - 1] !== true) {
+					fullGrid[y - 1][x - 1] += 1;
+				}
+				// west
+				if (x - 1 !== -1 && fullGrid[y][x - 1] !== true) {
+					fullGrid[y][x - 1] += 1;
+				}
+				// north west
+				if (y + 1 !== height && x - 1 !== -1 && fullGrid[y + 1][x - 1] !== true) {
+					fullGrid[y + 1][x - 1] += 1;
+				}
+			}
+		}
+	}
+	return fullGrid;
+}
