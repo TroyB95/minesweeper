62a37711285b2ef525613624cfbfd02d40c4df83
Troy Budd <troy_budd95@msn.com>
2020-02-18 10:59:48 +0000

    TB - Added mutateTrackingArray function

--- src/gameFunctions/gridHelpers.js
+++ src/gameFunctions/gridHelpers.js
@@ -1,7 +1,12 @@
 export function checkForBomb(value, e) {
-    if (value === true) {
-        e.target.innerHTML = "BOMB";
-        alert("YOU HAVE HIT ABOMB");
-    }
+	if (value === true) {
+		e.target.innerHTML = "BOMB";
+		alert("YOU HAVE HIT ABOMB");
+	}
 }
 
+export function mutateTrackingArray(y, x, trackingArr) {
+	let tileTrackingArr = [...trackingArr];
+	tileTrackingArr[y][x] = true;
+	return tileTrackingArr;
+}
