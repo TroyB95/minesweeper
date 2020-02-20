a3bebcf55861fe9973c4ae7d20d7dec487a225c3
Troy Budd <troy_budd95@msn.com>
2020-02-20 16:02:39 +0000

    TB - Added global state, Hooks, reducer

---
+++ src/globalState/index.js
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from "react";
+
+import types from "./types";
+
+const initialState = { tilesTurntCounter: 0 };
+const store = createContext(initialState);
+const { Provider } = store;
+
+const StateProvider = ({ children }) => {
+	const [state, dispatch] = useReducer((state, action) => {
+		switch (action.type) {
+			case types.INCREMENT_COUNT:
+				return {
+					tilesTurntCounter: state.tilesTurntCounter + 1,
+				};
+			//   case 'action description':
+			//     const newState = // do something with the action
+			//     return newState;
+			default:
+				throw new Error();
+		}
+	}, initialState);
+
+	return <Provider value={{ state, dispatch }}>{children}</Provider>;
+};
+
+export { store, StateProvider };
