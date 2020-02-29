0c28994d6deeb4cbf48df6b5214ae0cd43a7cd86
Troy Budd <troy_budd95@msn.com>
2020-02-29 08:33:46 +0000

    TB - Added test for generateCoordinate

--- src/gameFunctions/gridGeneration.test.js
+++ src/gameFunctions/gridGeneration.test.js
@@ -1,8 +1,16 @@
-import { create2DArray } from "./gridGeneration";
+import { create2DArray, generateCoordinate } from "./gridGeneration";
 
 describe("create2DArray", () => {
 	it("should return an array with as many nested arrays as the row argument specifys", () => {
 		expect(create2DArray(3)).toStrictEqual([[], [], []]);
 		expect(create2DArray(8)).toStrictEqual([[], [], [], [], [], [], [], []]);
+	});
+});
+
+describe("generateCoordinate", () => {
+	it("should generate 2 coordinates, between 0 and (gridDimensions - 1)", () => {
+		let coordinate = generateCoordinate(5);
+		expect(coordinate).toBeGreaterThanOrEqual(0);
+		expect(coordinate).toBeLessThan(5);
 	});
 });
