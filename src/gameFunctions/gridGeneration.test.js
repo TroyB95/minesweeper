1a3f07ca231e3713cf868c5389d0cc42b6d27f1c
Troy Budd <troy_budd95@msn.com>
2020-02-29 08:57:31 +0000

    TB - Test for generateBombs

--- src/gameFunctions/gridGeneration.test.js
+++ src/gameFunctions/gridGeneration.test.js
@@ -1,4 +1,4 @@
-import { create2DArray, generateCoordinate, compareCoords } from "./gridGeneration";
+import { create2DArray, generateCoordinate, compareCoords, generateBombs } from "./gridGeneration";
 
 describe("create2DArray", () => {
 	it("should return an array with as many nested arrays as the row argument specifys", () => {
@@ -37,5 +37,20 @@ describe("compareCoords", () => {
 				[[1, 2]]
 			)
 		).toBe(true);
+	});
+});
+
+describe("generateBombs", () => {
+	let generatedBombs = generateBombs(5, 5, 3);
+
+	it("should generate an array with as many sets of coordinates as bombs", () => {
+		expect(generateBombs).toHaveLength(3);
+	});
+
+	test.each(generatedBombs)("check range of each coordinate. Expect between 0 and 4", (a, b) => {
+		expect(a).toBeGreaterThanOrEqual(0);
+		expect(a).toBeLessThan(5);
+		expect(b).toBeGreaterThanOrEqual(0);
+		expect(b).toBeLessThan(5);
 	});
 });
