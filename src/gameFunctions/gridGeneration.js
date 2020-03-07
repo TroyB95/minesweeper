63df058397bfb531bb59cf9f5638d19da058f7f0
TroyB <troy_budd95@msn.com>
2020-03-07 18:27:36 +0000

    TB - Added js docs for generateCoordinate

--- src/gameFunctions/gridGeneration.js
+++ src/gameFunctions/gridGeneration.js
@@ -3,102 +3,106 @@
  * @param {string} rows - How many nested arrays.
  */
 export function create2DArray(rows) {
-	let arr = [];
-	for (let i = 0; i < rows; i++) {
-		arr[i] = [];
-	}
-	return arr;
+  let arr = [];
+  for (let i = 0; i < rows; i++) {
+    arr[i] = [];
+  }
+  return arr;
 }
 
+/**
+ * Generates number between 0 and max width of grid.
+ * @param {number} gridDimensions - Max width of grid.
+ */
 export function generateCoordinate(gridDimension) {
-	return Math.round(Math.random() * (gridDimension - 1));
+  return Math.round(Math.random() * (gridDimension - 1));
 }
 
 export function compareCoords(array1, array2) {
-	if (!array2) return false;
+  if (!array2) return false;
 
-	if (array1.toString().includes(array2.toString())) {
-		return true;
-	}
+  if (array1.toString().includes(array2.toString())) {
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 export function generateBombs(width, height, bombs) {
-	let bombsCoordArr = [];
+  let bombsCoordArr = [];
 
-	while (bombsCoordArr.length < bombs) {
-		let coordArr = [generateCoordinate(height), generateCoordinate(width)];
-		if (!compareCoords(bombsCoordArr, coordArr)) {
-			bombsCoordArr.push(coordArr);
-		}
-	}
-	return bombsCoordArr;
+  while (bombsCoordArr.length < bombs) {
+    let coordArr = [generateCoordinate(height), generateCoordinate(width)];
+    if (!compareCoords(bombsCoordArr, coordArr)) {
+      bombsCoordArr.push(coordArr);
+    }
+  }
+  return bombsCoordArr;
 }
 
 export function setBasicGrid(width, height, gridArr, value = 0) {
-	let filledGridArr = [...gridArr];
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			filledGridArr[y][x] = value;
-		}
-	}
-	return filledGridArr;
+  let filledGridArr = [...gridArr];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      filledGridArr[y][x] = value;
+    }
+  }
+  return filledGridArr;
 }
 
 export function generateGrid(width, height, bombs) {
-	let gridArr = setBasicGrid(width, height, create2DArray(height));
-	const bombsCoordArr = generateBombs(width, height, bombs);
+  let gridArr = setBasicGrid(width, height, create2DArray(height));
+  const bombsCoordArr = generateBombs(width, height, bombs);
 
-	(function plotBombs() {
-		bombsCoordArr.forEach(locationArr => {
-			gridArr[locationArr[0]][locationArr[1]] = true;
-		});
-	})();
+  (function plotBombs() {
+    bombsCoordArr.forEach(locationArr => {
+      gridArr[locationArr[0]][locationArr[1]] = true;
+    });
+  })();
 
-	return incrementAroundBombs(width, height, gridArr);
+  return incrementAroundBombs(width, height, gridArr);
 }
 
 export function incrementAroundBombs(width, height, gridArr) {
-	let fullGrid = [...gridArr];
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			// If bomb increment all coords around it
-			if (fullGrid[y][x] === true) {
-				// north
-				if (y + 1 !== height && fullGrid[y + 1][x] !== true) {
-					fullGrid[y + 1][x] += 1;
-				}
-				// north east
-				if (y + 1 !== height && x + 1 !== width && fullGrid[y + 1][x + 1] !== true) {
-					fullGrid[y + 1][x + 1] += 1;
-				}
-				// east
-				if (x + 1 !== width && fullGrid[y][x + 1] !== true) {
-					fullGrid[y][x + 1] += 1;
-				}
-				// south east
-				if (y - 1 !== -1 && x + 1 !== width && fullGrid[y - 1][x + 1] !== true) {
-					fullGrid[y - 1][x + 1] += 1;
-				}
-				// south
-				if (y - 1 !== -1 && fullGrid[y - 1][x] !== true) {
-					fullGrid[y - 1][x] += 1;
-				}
-				// south west
-				if (y - 1 !== -1 && x - 1 !== -1 && fullGrid[y - 1][x - 1] !== true) {
-					fullGrid[y - 1][x - 1] += 1;
-				}
-				// west
-				if (x - 1 !== -1 && fullGrid[y][x - 1] !== true) {
-					fullGrid[y][x - 1] += 1;
-				}
-				// north west
-				if (y + 1 !== height && x - 1 !== -1 && fullGrid[y + 1][x - 1] !== true) {
-					fullGrid[y + 1][x - 1] += 1;
-				}
-			}
-		}
-	}
-	return fullGrid;
+  let fullGrid = [...gridArr];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      // If bomb increment all coords around it
+      if (fullGrid[y][x] === true) {
+        // north
+        if (y + 1 !== height && fullGrid[y + 1][x] !== true) {
+          fullGrid[y + 1][x] += 1;
+        }
+        // north east
+        if (y + 1 !== height && x + 1 !== width && fullGrid[y + 1][x + 1] !== true) {
+          fullGrid[y + 1][x + 1] += 1;
+        }
+        // east
+        if (x + 1 !== width && fullGrid[y][x + 1] !== true) {
+          fullGrid[y][x + 1] += 1;
+        }
+        // south east
+        if (y - 1 !== -1 && x + 1 !== width && fullGrid[y - 1][x + 1] !== true) {
+          fullGrid[y - 1][x + 1] += 1;
+        }
+        // south
+        if (y - 1 !== -1 && fullGrid[y - 1][x] !== true) {
+          fullGrid[y - 1][x] += 1;
+        }
+        // south west
+        if (y - 1 !== -1 && x - 1 !== -1 && fullGrid[y - 1][x - 1] !== true) {
+          fullGrid[y - 1][x - 1] += 1;
+        }
+        // west
+        if (x - 1 !== -1 && fullGrid[y][x - 1] !== true) {
+          fullGrid[y][x - 1] += 1;
+        }
+        // north west
+        if (y + 1 !== height && x - 1 !== -1 && fullGrid[y + 1][x - 1] !== true) {
+          fullGrid[y + 1][x - 1] += 1;
+        }
+      }
+    }
+  }
+  return fullGrid;
 }
