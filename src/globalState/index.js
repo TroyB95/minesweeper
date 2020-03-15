c10c16c8c7eedeca02c3bfe26435373111d6ed05
TroyB <troy_budd95@msn.com>
2020-03-15 09:15:07 +0000

    TB - Added RESET_GRID_OPTIONS action

--- src/globalState/index.js
+++ src/globalState/index.js
@@ -3,49 +3,54 @@ import React, { createContext, useReducer } from "react";
 import types from "./types";
 
 const initialState = {
-	tilesTurntCounter: 0,
-	flaggedLocations: new Set(),
-	gameOptions: { bombCount: 0, gridSize: 0 },
+  tilesTurntCounter: 0,
+  flaggedLocations: new Set(),
+  gameOptions: { bombCount: 0, gridSize: 0 },
 };
 
 const store = createContext(initialState);
 const { Provider } = store;
 
 const StateProvider = ({ children }) => {
-	const [state, dispatch] = useReducer((state, action) => {
-		switch (action.type) {
-			case types.INCREMENT_COUNT:
-				return {
-					...state,
-					tilesTurntCounter: state.tilesTurntCounter + 1,
-				};
-			case types.RESET_COUNT:
-				return {
-					...state,
-					tilesTurntCounter: 0,
-				};
-			case types.UPDATE_FLAGGED_LOCATIONS:
-				return {
-					...state,
-					flaggedLocations: action.payload,
-				};
-			case types.RESET_FLAGGED_LOCATIONS:
-				return {
-					...state,
-					flaggedLocations: new Set(),
-				};
-			case types.UPDATE_GRID_OPTIONS:
-				return {
-					...state,
-					gameOptions: { ...state.gameOptions, ...action.payload },
-				};
-			
-			default:
-				throw new Error();
-		}
-	}, initialState);
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case types.INCREMENT_COUNT:
+        return {
+          ...state,
+          tilesTurntCounter: state.tilesTurntCounter + 1,
+        };
+      case types.RESET_COUNT:
+        return {
+          ...state,
+          tilesTurntCounter: 0,
+        };
+      case types.UPDATE_FLAGGED_LOCATIONS:
+        return {
+          ...state,
+          flaggedLocations: action.payload,
+        };
+      case types.RESET_FLAGGED_LOCATIONS:
+        return {
+          ...state,
+          flaggedLocations: new Set(),
+        };
+      case types.UPDATE_GRID_OPTIONS:
+        return {
+          ...state,
+          gameOptions: { ...state.gameOptions, ...action.payload },
+        };
+      case types.RESET_GRID_OPTIONS:
+        return {
+          ...state,
+          gameOptions: { bombCount: 0, gridSize: 0 },
+        };
 
-	return <Provider value={{ state, dispatch }}>{children}</Provider>;
+      default:
+        throw new Error();
+    }
+  }, initialState);
+
+  return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
 
 export { store, StateProvider };
