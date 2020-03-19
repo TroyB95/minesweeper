f07993fe916169a3d0a4f7f827e7b8668bad8685
TroyB <troy_budd95@msn.com>
2020-03-19 08:33:56 +0000

    TB - Conditionally disable input, Destructured gridOptions

--- src/Components/StartScreen/index.js
+++ src/Components/StartScreen/index.js
@@ -5,95 +5,98 @@ import types from "../../globalState/types";
 import { store } from "../../globalState";
 
 const StartScreenModalBackground = styled.div`
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 
-	background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.8);
 
-	position: absolute;
+  position: absolute;
 
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StartScreenModal = styled.div`
-	width: 50%;
-	height: 50%;
+  width: 50%;
+  height: 50%;
 
-	background: grey;
+  background: grey;
 
-	display: flex;
-	flex-wrap: wrap;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const OptionsForm = styled.form`
-	width: 80%;
-	height: 80%;
+  width: 80%;
+  height: 80%;
 
-	display: flex;
-	flex-wrap: wrap;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-around;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const OptionsInputDiv = styled.div`
-	width: 80%;
-	height: 15%;
+  width: 80%;
+  height: 15%;
 
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 function StartScreen(props) {
-	const [gridOptions, setGridOptions] = useState({ gridSize: 0, bombCount: 0 });
-	const globalState = useContext(store);
-	const { dispatch } = globalState;
+  const [gridOptions, setGridOptions] = useState({ gridSize: 0, bombCount: 0 });
+  const globalState = useContext(store);
+  const { dispatch } = globalState;
 
-	return (
-		<StartScreenModalBackground>
-			<StartScreenModal>
-				<OptionsForm
-					onSubmit={e => {
-						e.preventDefault();
-						dispatch({ type: types.UPDATE_GRID_OPTIONS, payload: gridOptions });
-						props.setOptionsSubmitted(true);
-					}}
-				>
-					<OptionsInputDiv>
-						<label>Grid size: </label>
-						<input
-							type="number"
-							min="1"
-							max="25"
-							value={gridOptions.gridSize}
-							onChange={e => {
-								setGridOptions({ ...gridOptions, gridSize: Number(e.target.value) });
-							}}
-						></input>
-					</OptionsInputDiv>
-					<OptionsInputDiv>
-						<label>Number of bombs: </label>
-						<input
-							type="number"
-							min="1"
-							max="25"
-							value={gridOptions.bombCount}
-							onChange={e => {
-								setGridOptions({ ...gridOptions, bombCount: Number(e.target.value) });
-							}}
-						></input>
-					</OptionsInputDiv>
-					<input type="submit" value="Submit" />
-				</OptionsForm>
-			</StartScreenModal>
-		</StartScreenModalBackground>
-	);
+  const { gridSize, bombCount } = gridOptions;
+
+  return (
+    <StartScreenModalBackground>
+      <StartScreenModal>
+        <OptionsForm
+          onSubmit={e => {
+            e.preventDefault();
+            dispatch({ type: types.UPDATE_GRID_OPTIONS, payload: gridOptions });
+            props.setOptionsSubmitted(true);
+          }}
+        >
+          <OptionsInputDiv>
+            <label>Grid size: </label>
+            <input
+              type="number"
+              min="10"
+              max="50"
+              value={gridSize}
+              onChange={e => {
+                setGridOptions({ ...gridOptions, gridSize: Number(e.target.value) });
+              }}
+            ></input>
+          </OptionsInputDiv>
+          <OptionsInputDiv>
+            <label>Number of bombs: </label>
+            <input
+              disabled={gridSize > 0 ? false : true}
+              type="number"
+              min="3"
+              max="100"
+              value={bombCount}
+              onChange={e => {
+                setGridOptions({ ...gridOptions, bombCount: Number(e.target.value) });
+              }}
+            ></input>
+          </OptionsInputDiv>
+          <input type="submit" value="Submit" />
+        </OptionsForm>
+      </StartScreenModal>
+    </StartScreenModalBackground>
+  );
 }
 
 export default StartScreen;
