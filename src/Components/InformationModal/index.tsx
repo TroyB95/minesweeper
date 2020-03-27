67e6d90c5a1713ccbe2a2e5a1197fd42d0cfce33
Troy <troy_budd95@msn.com>
2020-03-27 14:21:12 +0000

    TB - Converted file to typescript

--- src/Components/InformationModal/index.tsx
+++ src/Components/InformationModal/index.tsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const ModalBackground = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.8);
 
-    position: absolute;
+  position: absolute;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const ModalBody = styled.div`
-    width: 50%;
-    height: 50%;
+  width: 50%;
+  height: 50%;
 
-    background: grey;
+  background: grey;
 
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 type ModalProps = {
-        resetGame: Function;
-    type: string;
+  resetGame: Function;
+  type: string;
 };
 
 const InformationModal = ({ resetGame, type }: ModalProps) => {
-    return (
-        <ModalBackground>
-            <ModalBody>
-                {type === 'win' && (
-                    <>
-                        <h1>CONGRATS YOU HAVE WON</h1>
-                        <button onClick={() => resetGame()}>Play Again?</button>
-                    </>
-                )}
-                {type === 'loss' && (
-                    <>
-                        <h1>YOU HAVE LOST!</h1>
-                        <button onClick={() => resetGame()}>Play Again?</button>
-                    </>
-                )}
-            </ModalBody>
-        </ModalBackground>
-    );
+  return (
+    <ModalBackground>
+      <ModalBody>
+        {type === "win" && (
+          <>
+            <h1>CONGRATS YOU HAVE WON</h1>
+            <button onClick={() => resetGame()}>Play Again?</button>
+          </>
+        )}
+        {type === "loss" && (
+          <>
+            <h1>YOU HAVE LOST!</h1>
+            <button onClick={() => resetGame()}>Play Again?</button>
+          </>
+        )}
+      </ModalBody>
+    </ModalBackground>
+  );
 };
 
 export default InformationModal;
