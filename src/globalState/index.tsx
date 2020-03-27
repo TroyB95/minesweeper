68c674efaa3c69544017ff48ae64e7dd99ba3190
Troy <troy_budd95@msn.com>
2020-03-27 14:21:35 +0000

    TB - Converted file to TS

--- src/globalState/index.tsx
+++ src/globalState/index.tsx
@@ -1,48 +1,49 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, FunctionComponent } from "react";
 
 import types from "./types";
 
 const initialState = {
   tilesTurntCounter: 0,
   flaggedLocations: new Set(),
-  gameOptions: { bombCount: 0, gridSize: 0 },
+  gameOptions: { bombCount: 0, gridSize: 0 }
 };
 
 const store = createContext(initialState);
 const { Provider } = store;
 
-const StateProvider = ({ children }) => {
+const StateProvider: FunctionComponent = ({ children }) => {
   const [state, dispatch] = useReducer((state, action) => {
     switch (action.type) {
       case types.INCREMENT_COUNT:
         return {
           ...state,
-          tilesTurntCounter: state.tilesTurntCounter + (action.payload ? action.payload : 1),
+          tilesTurntCounter:
+            state.tilesTurntCounter + (action.payload ? action.payload : 1)
         };
       case types.RESET_COUNT:
         return {
           ...state,
-          tilesTurntCounter: 0,
+          tilesTurntCounter: 0
         };
       case types.UPDATE_FLAGGED_LOCATIONS:
         return {
           ...state,
-          flaggedLocations: action.payload,
+          flaggedLocations: action.payload
         };
       case types.RESET_FLAGGED_LOCATIONS:
         return {
           ...state,
-          flaggedLocations: new Set(),
+          flaggedLocations: new Set()
         };
       case types.UPDATE_GRID_OPTIONS:
         return {
           ...state,
-          gameOptions: { ...state.gameOptions, ...action.payload },
+          gameOptions: { ...state.gameOptions, ...action.payload }
         };
       case types.RESET_GRID_OPTIONS:
         return {
           ...state,
-          gameOptions: { bombCount: 0, gridSize: 0 },
+          gameOptions: { bombCount: 0, gridSize: 0 }
         };
 
       default:
