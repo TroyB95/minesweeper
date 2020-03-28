d5adaa65e98e297f0a4624ab40b9b51199bc401a
Troy <troy_budd95@msn.com>
2020-03-28 14:45:46 +0000

    TB - Added TS

--- src/globalState/index.tsx
+++ src/globalState/index.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useReducer, FunctionComponent } from "react";
 
-import types from "./types";
+import types, { ChatActionTypes } from "./types";
 
 const initialState = {
   tilesTurntCounter: 0,
@@ -8,54 +8,62 @@ const initialState = {
   gameOptions: { bombCount: 0, gridSize: 0 }
 };
 
-const store = createContext(initialState);
-const { Provider } = store;
+interface IContextProps {
+  state: StateType;
+  dispatch: ({ type, payload }: { type: string; payload: any }) => void;
+}
 
 type StateType = {
-  tilesTurntCounter: number;
-  flaggedLocations: Set<Array<number>>;
-  gameOptions: Object;
+  tilesTurntCounter?: number;
+  flaggedLocations?: Set<Array<number>>;
+  gameOptions?: Object;
 };
 
-const StateProvider: FunctionComponent = ({ children }) => {
-  const [state, dispatch] = useReducer((state: StateType, action: String) => {
-    switch (action.type) {
-      case types.INCREMENT_COUNT:
-        return {
-          ...state,
-          tilesTurntCounter:
-            state.tilesTurntCounter + (action.payload ? action.payload : 1)
-        };
-      case types.RESET_COUNT:
-        return {
-          ...state,
-          tilesTurntCounter: 0
-        };
-      case types.UPDATE_FLAGGED_LOCATIONS:
-        return {
-          ...state,
-          flaggedLocations: action.payload
-        };
-      case types.RESET_FLAGGED_LOCATIONS:
-        return {
-          ...state,
-          flaggedLocations: new Set()
-        };
-      case types.UPDATE_GRID_OPTIONS:
-        return {
-          ...state,
-          gameOptions: { ...state.gameOptions, ...action.payload }
-        };
-      case types.RESET_GRID_OPTIONS:
-        return {
-          ...state,
-          gameOptions: { bombCount: 0, gridSize: 0 }
-        };
+const store = createContext({} as IContextProps);
+const { Provider } = store;
 
-      default:
-        throw new Error();
-    }
-  }, initialState);
+const StateProvider: FunctionComponent = ({ children }) => {
+  const [state, dispatch] = useReducer(
+    (state: StateType, action: ChatActionTypes) => {
+      switch (action.type) {
+        case types.INCREMENT_COUNT:
+          return {
+            ...state,
+            tilesTurntCounter:
+              state.tilesTurntCounter + (action.payload ? action.payload : 1)
+          };
+        case types.RESET_COUNT:
+          return {
+            ...state,
+            tilesTurntCounter: 0
+          };
+        case types.UPDATE_FLAGGED_LOCATIONS:
+          return {
+            ...state,
+            flaggedLocations: action.payload
+          };
+        case types.RESET_FLAGGED_LOCATIONS:
+          return {
+            ...state,
+            flaggedLocations: new Set()
+          };
+        case types.UPDATE_GRID_OPTIONS:
+          return {
+            ...state,
+            gameOptions: { ...state.gameOptions, ...action.payload }
+          };
+        case types.RESET_GRID_OPTIONS:
+          return {
+            ...state,
+            gameOptions: { bombCount: 0, gridSize: 0 }
+          };
+
+        default:
+          throw new Error();
+      }
+    },
+    initialState
+  );
 
   return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
