b99d2ca5cb3b4a8444677d61de81855cbc74d647
Troy <troy_budd95@msn.com>
2020-03-30 09:04:12 +0100

    TB - Linted / formatted correctly

--- src/Components/TimerBar/index.js
+++ src/Components/TimerBar/index.js
@@ -3,31 +3,35 @@ import styled from "styled-components";
 import Timer from "react-compound-timer";
 
 const TimerContainer = styled.div`
-	width: 80%;
-	height: 10%;
+  width: 80%;
+  height: 10%;
 `;
 
 function TimerBar(props) {
-	return (
-		<TimerContainer>
-			<Timer>
-				{({ resume, pause, reset }) => {
-					if (props.restart === true) {
-						reset();
-						pause();
-					}
-					return (
-						<>
-							<Timer.Minutes formatValue={value => `${value}m :`}></Timer.Minutes>
-							<Timer.Seconds formatValue={value => ` ${value}s`}></Timer.Seconds>
-							<button onClick={pause}>Pause</button>
-							<button onClick={resume}>Resume</button>
-						</>
-					);
-				}}
-			</Timer>
-		</TimerContainer>
-	);
+  return (
+    <TimerContainer>
+      <Timer>
+        {({ resume, pause, reset }) => {
+          if (props.restart === true) {
+            reset();
+            pause();
+          }
+          return (
+            <>
+              <Timer.Minutes
+                formatValue={value => `${value}m :`}
+              ></Timer.Minutes>
+              <Timer.Seconds
+                formatValue={value => ` ${value}s`}
+              ></Timer.Seconds>
+              <button onClick={pause}>Pause</button>
+              <button onClick={resume}>Resume</button>
+            </>
+          );
+        }}
+      </Timer>
+    </TimerContainer>
+  );
 }
 
 export default TimerBar;
