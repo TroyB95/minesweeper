46a7dd6c7b846b524488d76aeeed1288b5c03e47
Troy <troy_budd95@msn.com>
2020-04-03 09:52:26 +0100

    TB - Pulled StyledComponents into new file

--- src/Components/InformationModal/index.tsx
+++ src/Components/InformationModal/index.tsx
@@ -1,30 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 
-const ModalBackground = styled.div`
-  width: 100%;
-  height: 100%;
-
-  background-color: rgba(0, 0, 0, 0.8);
-
-  position: absolute;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const ModalBody = styled.div`
-  width: 50%;
-  height: 50%;
-
-  background: grey;
-
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+import { ModalBackground, ModalBody } from "./InformationModal.styled";
 
 type ModalProps = {
   resetGame: Function;
