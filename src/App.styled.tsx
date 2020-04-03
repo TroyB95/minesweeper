d4372d9863894506915c899290845d60042b3a7a
Troy <troy_budd95@msn.com>
2020-04-03 09:57:03 +0100

    TB - Pulled StyledComponents into new file

---
+++ src/App.styled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const GridContainer = styled.div`
+  width: 80%;
+  height: 80%;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from "react";
-import styled from "styled-components";
 import uniqid from "uniqid";
 import { store } from "./globalState";
 import types from "./globalState/types";
@@ -16,26 +15,13 @@ import {
   flipBlankTiles
 } from "./gameFunctions/gridHelpers";
 
+import { GridContainer, PageContainer } from "./App.styled";
+
 import TimerBar from "./Components/TimerBar";
 import StartScreen from "./Components/StartScreen";
 import InformationModal from "./Components/InformationModal";
 import GridSquare from "./Components/GridSquare";
 import GridRow from "./Components/GridRow";
-
-const PageContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const GridContainer = styled.div`
-  width: 80%;
-  height: 80%;
-`;
 
 function App() {
   const globalState = useContext(store);
