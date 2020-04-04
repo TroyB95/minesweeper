064d17e6b06b9f523b6270cfdd024041f4804fca
Troy <troy_budd95@msn.com>
2020-04-04 14:07:16 +0100

    TB - Added playTime into props and modal

--- src/Components/InformationModal/index.tsx
+++ src/Components/InformationModal/index.tsx
@@ -5,21 +5,24 @@ import { ModalBackground, ModalBody } from "./InformationModal.styled";
 type ModalProps = {
   resetGame: Function;
   type: string;
+  playTime: number;
 };
 
-const InformationModal = ({ resetGame, type }: ModalProps) => {
+const InformationModal = ({ resetGame, type, playTime }: ModalProps) => {
   return (
     <ModalBackground>
       <ModalBody>
         {type === "win" && (
           <>
             <h1>CONGRATS YOU HAVE WON</h1>
+            <h3>Total time played: {playTime}</h3>
             <button onClick={() => resetGame()}>Play Again?</button>
           </>
         )}
         {type === "loss" && (
           <>
             <h1>YOU HAVE LOST!</h1>
+            <h3>Total time played: {playTime}</h3>
             <button onClick={() => resetGame()}>Play Again?</button>
           </>
         )}
