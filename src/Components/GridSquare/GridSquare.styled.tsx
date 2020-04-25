ef9c8dada78f88344e21a2ee51b61cd5170a5a4a
Troy <troy_budd95@msn.com>
2020-04-25 10:20:20 +0100

    TB - Added TODO for hover effect on tiles

--- src/Components/GridSquare/GridSquare.styled.tsx
+++ src/Components/GridSquare/GridSquare.styled.tsx
@@ -6,7 +6,7 @@ export const GridSection = styled.div<{
 }>`
   ${({ theme: { colour, font } }) => `
   font-family: ${font.bold};
-  border-right:3px solid ${colour.background};
+  border-right:3px solid ${colour.background}; 
 `}
 
   flex-grow: 1;
@@ -20,7 +20,12 @@ export const GridSection = styled.div<{
   width: ${(props) => 100 / props.width + "%"};
   height: 100%;
 `;
-
+/* :hover {
+    cursor: ${(props) => props.pointer};
+    background: ${(props) => props.backgroundColour};
+  }
+  */
+// TODO: Add hover props and background for turnt tiles ^^
 export const TileImage = styled.img`
   width: 90%;
   height: 90%;
