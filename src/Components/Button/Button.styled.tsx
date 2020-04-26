ec1ab7d73ded5b5ec20d9b7d124c0bd92734ff89
Troy <troy_budd95@msn.com>
2020-04-26 09:27:48 +0100

    TB - Added config to button

--- src/Components/Button/Button.styled.tsx
+++ src/Components/Button/Button.styled.tsx
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<{
+  fontSize?: string;
+  width?: string;
+  height?: string;
+}>`
   ${({ theme: { font, colour } }) => `
     font-family: ${font.family};
     background: ${colour.mainFont};
    
-
   text-transform: uppercase;
   text-decoration: none;
+
+  cursor: pointer;
 
   color: black;
   padding: 4px;
   border: 1px solid ${colour.background};
   display: inline-block;
+
   transition: all 0.4s ease 0s;
 
   :hover {
@@ -21,6 +27,11 @@ const StyledButton = styled.button`
     transition: all 0.4s ease 0s;
   }
    `}
+
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "12px")};
+  width: ${(props) => (props.width ? props.width : "")};
+  height: ${(props) => (props.height ? props.height : "")};
+
 `;
 
 export { StyledButton };
