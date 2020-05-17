652a2f5a2e436370437ff889bc8149355cbd5fb2
Troy <troy_budd95@msn.com>
2020-05-17 08:50:26 +0100

    TB - Added test for mutateTrackingArray

--- src/gameFunctions/gridHelpers.test.ts
+++ src/gameFunctions/gridHelpers.test.ts
@@ -1,4 +1,4 @@
-import { checkForBomb } from "./gridHelpers";
+import { checkForBomb, mutateTrackingArray } from "./gridHelpers";
 
 describe("checkForBomb", () => {
   it("should return true is value is true", () => {
@@ -9,5 +9,20 @@ describe("checkForBomb", () => {
     expect(checkForBomb(1)).toBe(false);
     expect(checkForBomb(false)).toBe(false);
     expect(checkForBomb("testString")).toBe(false);
+  });
+});
+
+describe("mutateTrackingArray", () => {
+  it("should mutate the array and add the value to the correct x, y cordinate", () => {
+    let trackingArr = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    expect(mutateTrackingArray(0, 2, trackingArr, true)).toEqual([
+      [0, 0, true],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
   });
 });
