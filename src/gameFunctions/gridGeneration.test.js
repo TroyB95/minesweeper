37c231cbd7b8027602bd00f8b273b420d8330562
Troy <troy_budd95@msn.com>
2020-05-17 08:16:39 +0100

    TB - Added test for incrementAroundBombs

--- src/gameFunctions/gridGeneration.test.js
+++ src/gameFunctions/gridGeneration.test.js
@@ -4,6 +4,7 @@ import {
   compareCoords,
   generateBombs,
   setBasicGrid,
+  incrementAroundBombs,
 } from "./gridGeneration";
 
 describe("create2DArray", () => {
@@ -53,22 +54,45 @@ describe("generateBombs", () => {
     expect(generateBombs).toHaveLength(3);
   });
 
-  test.each(generatedBombs)("check range of each coordinate. Expect between 0 and 4", (a, b) => {
-    expect(a).toBeGreaterThanOrEqual(0);
-    expect(a).toBeLessThan(5);
-    expect(b).toBeGreaterThanOrEqual(0);
-    expect(b).toBeLessThan(5);
-  });
+  test.each(generatedBombs)(
+    "check range of each coordinate. Expect between 0 and 4",
+    (a, b) => {
+      expect(a).toBeGreaterThanOrEqual(0);
+      expect(a).toBeLessThan(5);
+      expect(b).toBeGreaterThanOrEqual(0);
+      expect(b).toBeLessThan(5);
+    }
+  );
 });
 
 describe("setBasicGrid", () => {
   let basicGrid = setBasicGrid(5, create2DArray(5), 0);
 
-  test.each(basicGrid)("should fill the grid with the value 0", (a, b, c, d, e) => {
-    expect(a).toBe(0);
-    expect(b).toBe(0);
-    expect(c).toBe(0);
-    expect(d).toBe(0);
-    expect(e).toBe(0);
+  test.each(basicGrid)(
+    "should fill the grid with the value 0",
+    (a, b, c, d, e) => {
+      expect(a).toBe(0);
+      expect(b).toBe(0);
+      expect(c).toBe(0);
+      expect(d).toBe(0);
+      expect(e).toBe(0);
+    }
+  );
+});
+
+describe("incrementArondBombs", () => {
+  it.only("should increment three squares around the bomb (the true value within the grid)", () => {
+    let basicGrid = [];
+    basicGrid[0] = new Array(true, 0, 0, 0, 0);
+    basicGrid[1] = new Array(0, 0, 0, 0, 0);
+    basicGrid[2] = new Array(0, 0, 0, 0, 0);
+    basicGrid[3] = new Array(0, 0, 0, 0, 0);
+    basicGrid[4] = new Array(0, 0, 0, 0, 0);
+
+    let incrementedArray = incrementAroundBombs(5, basicGrid);
+
+    expect(incrementedArray[0][1]).toBe(1);
+    expect(incrementedArray[1][0]).toBe(1);
+    expect(incrementedArray[1][1]).toBe(1);
   });
 });
