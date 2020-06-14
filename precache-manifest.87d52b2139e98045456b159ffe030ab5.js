e1c012fb3698fce811e6070897d6b580a486fcb8
Troy Budd <troy.budd@ovoenergy.com>
2020-06-14 09:46:49 +0100

    TB - Updated page

---
+++ precache-manifest.87d52b2139e98045456b159ffe030ab5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d5ed99d9a2e8c67ba192a098b77a6a",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "828eae8aa91e5333505c",
+    "url": "/minesweeper/static/css/main.0a4c25d3.chunk.css"
+  },
+  {
+    "revision": "e62dc8621d5baa661855",
+    "url": "/minesweeper/static/js/2.824458df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minesweeper/static/js/2.824458df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828eae8aa91e5333505c",
+    "url": "/minesweeper/static/js/main.8a2e25b5.chunk.js"
+  },
+  {
+    "revision": "6fc47614888be104d4f8",
+    "url": "/minesweeper/static/js/runtime-main.e1079ecc.js"
+  },
+  {
+    "revision": "9122299e476671f970e1670bd7b900c8",
+    "url": "/minesweeper/static/media/FrederickatheGreat-Regular.9122299e.ttf"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/minesweeper/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "1dacf0894593bb41b1bb04caf96a0f2e",
+    "url": "/minesweeper/static/media/bomb.1dacf089.svg"
+  },
+  {
+    "revision": "a3d33fe2a18f743c1c23845df799949a",
+    "url": "/minesweeper/static/media/button-select.a3d33fe2.mp3"
+  },
+  {
+    "revision": "c0ece8456e6a5c85a6efafeae08c51f7",
+    "url": "/minesweeper/static/media/explosion.c0ece845.mp3"
+  },
+  {
+    "revision": "22bb4aa62f17245a8badcac429347ba1",
+    "url": "/minesweeper/static/media/low-volume.22bb4aa6.svg"
+  },
+  {
+    "revision": "5fd330c2ce91b26f99b14e1489b17d59",
+    "url": "/minesweeper/static/media/mid-volume.5fd330c2.svg"
+  },
+  {
+    "revision": "33a1c2e3dc353d62042dded0eb55a799",
+    "url": "/minesweeper/static/media/rest-of-tiles-turn.33a1c2e3.mp3"
+  },
+  {
+    "revision": "5f8652153be2b16435885c04e4e10a91",
+    "url": "/minesweeper/static/media/round-complete.5f865215.mp3"
+  },
+  {
+    "revision": "ef59c71c5d48e4d6e130bb915d0552df",
+    "url": "/minesweeper/static/media/skull.ef59c71c.svg"
+  },
+  {
+    "revision": "31104fb28d030addab7739447563c384",
+    "url": "/minesweeper/static/media/speaker-mute.31104fb2.svg"
+  },
+  {
+    "revision": "cb5eedc9e34ec29999d8d0161bf4c179",
+    "url": "/minesweeper/static/media/speaker.cb5eedc9.svg"
+  },
+  {
+    "revision": "f4bd362594a78a9e60b11830e8f13853",
+    "url": "/minesweeper/static/media/tile-flag.f4bd3625.mp3"
+  },
+  {
+    "revision": "627557d524abc1763ec720888c19384d",
+    "url": "/minesweeper/static/media/tile-turn.627557d5.mp3"
+  }
+]);
